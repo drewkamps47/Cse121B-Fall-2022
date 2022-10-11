ab496f3fc414dc934d848ea6034b18e74a1b24a5
comkamps787 <64157435+comkamps787@users.noreply.github.com>
2022-10-10 23:03:26 -0600

    Init

--- README.md
+++
@@ -1,2 +0,0 @@
-# Cse121B-Fall-2022
-BYU-Idaho CSE 121B Course Work
--- Week03/scripts/main.js
+++ Week03/scripts/main.js
@@ -5,6 +5,3 @@ const toggleMenu = () => {
 
 document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
 
-// set current year in footer
-const currentDate = new Date();
-document.querySelector('#year').textContent = currentDate.getFullYear();
